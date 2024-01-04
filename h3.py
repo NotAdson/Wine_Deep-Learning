e9811ddd2ef5974aa6cc19a5f61706cf0575e653
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-01-04 18:43:00 +0000

    Sistema de deixar todos as colunas na mesma escala

--- h3.py
+++ h3.py
@@ -1,21 +1,37 @@
 import pandas as pd
 from tensorflow.keras import layers, Sequential
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 table = pd.read_csv("fuel.csv")
 
 train_data = table.sample(frac=0.6, random_state=0)
 valid_data = table.drop(train_data.index)
 
-list_max = train_data.max()
-list_min = train_data.min()
+# Identifique colunas categóricas
+categorical_cols = train_data.select_dtypes(include=['object']).columns
 
-train_data = (train_data - list_min) / (list_max - list_min)
-valid_data = (valid_data - list_min) / (list_max - list_min)
+# Crie DataFrames com colunas categóricas transformadas em colunas binárias
+train_data_dummies = pd.get_dummies(train_data, columns=categorical_cols)
+valid_data_dummies = pd.get_dummies(valid_data, columns=categorical_cols)
 
-x_train = train_data.drop("FE", axis=1)
-x_valid = valid_data.drop("FE", axis=1)
-y_train = valid_data["FE"]
+# Use o MinMaxScaler para normalizar os dados mantendo as estatísticas do conjunto de treinamento
+scaler = MinMaxScaler()
+train_data_scaled = scaler.fit_transform(train_data_dummies.drop("FE", axis=1))
+valid_data_scaled = scaler.transform(valid_data_dummies.drop("FE", axis=1))
+
+# Crie DataFrames normalizados
+train_data_normalized = pd.DataFrame(train_data_scaled, columns=train_data_dummies.columns[:-1])
+valid_data_normalized = pd.DataFrame(valid_data_scaled, columns=valid_data_dummies.columns[:-1])
+
+# Separar características e rótulos
+x_train = train_data_normalized
+y_train = train_data["FE"]
+
+x_valid = valid_data_normalized
 y_valid = valid_data["FE"]
+
+
 
 model = Sequential([
     layers.Dense(128, activation="relu", input_shape=[50]),
@@ -29,6 +45,13 @@ model.compile(
     loss = "mae"
 )
 
-history = model.fit()
+history = model.fit(
+    x_train, y_train,
+    validation_data=(x_valid, y_valid),
+    batch_size = 64,
+    epochs = 200
+)
 
-
+history_df = pd.DataFrame(history.history)
+history_df['loss'].plot()
+plt.show()
